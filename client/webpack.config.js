e448d0d8be8e480d42677f61df7317e298606ae4
fzhao888 <frank.zhao93@gmail.com>
2023-09-15 13:05:49 -0500

    Added scripts, server, build, install, client, start:dev to scripts in package.json'

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,9 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
@@ -44,6 +41,7 @@ module.exports = () => {
       icons: [
         {
           src: path.resolve('src/images/logo.png'),
+          size: '96x96',
           destination: path.join('assets', 'icons')
         }
       ]
