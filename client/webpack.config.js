d579a067471d3064f3ce567c062d42fe6c977019
fzhao888 <frank.zhao93@gmail.com>
2023-09-15 12:50:24 -0500

    Added webpackpwamanifest

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -36,7 +36,7 @@ module.exports = () => {
       inject: true,
       name: 'Just Another Text Editor',
       short_name: 'J.A.T.E',
-      description: 'A',
+      description: 'Another text editor for your daily needs!',
       background_color: '#225ca3',
       theme_color: '#225ca3',
       start_url: './',
